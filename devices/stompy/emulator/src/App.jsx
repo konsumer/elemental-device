f4cae61ed2bac7619af448c35fb5143c54ba0c4a
David Konsumer <konsumer@jetboystudio.com>
2021-07-09 15:31:25 -0700

    seperate RotaryEncoder

--- devices/stompy/emulator/src/App.jsx
+++ devices/stompy/emulator/src/App.jsx
@@ -1,43 +1,19 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Knob } from "react-rotary-knob"
+import * as skins from 'react-rotary-knob-skin-pack'
 
-function App() {
-  const [count, setCount] = useState(0)
+const KnobRotary = props => (
+  <Knob skin={skins.s15} {...props} />
+)
 
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <KnobRotary></KnobRotary>
+      <KnobRotary></KnobRotary>
+      <KnobRotary></KnobRotary>
+      <KnobRotary></KnobRotary>
     </div>
   )
 }
